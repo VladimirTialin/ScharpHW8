825527357efc04ccda6033a85730b350d34cae71
VladimirTialin <103684564+VladimirTialin@users.noreply.github.com>
2022-06-07 21:41:26 +0300

    Решили задачу 58

--- Example58/Program.cs
+++ Example58/Program.cs
@@ -7,7 +7,7 @@ int[,] NumberArray(int[,] numberArray)
 {
     
     for (var i = 0; i < numberArray.GetLength(0); i++)
-    {
+{
         for (var j = 0; j < numberArray.GetLength(1); j++)
         {
             numberArray[i, j] = new Random().Next(30, 90);
@@ -16,23 +16,47 @@ int[,] NumberArray(int[,] numberArray)
     return numberArray;
 }
 // Выводим массива случайных чисел
-void PrintArray(int[,] arrayToPrintMatrixOne,int[,] arrayToPrintMatrixTwo)
+void PrintMatrixs(int[,] arrayToPrintMatrixOne,
+                  int[,] arrayToPrintMatrixTwo,
+                  string nameMatrixOne,
+                  string nameMatrixTwo)              
 {
+    Console.SetCursorPosition((arrayToPrintMatrixOne.GetLength(1)*2+5)-nameMatrixOne.Length,Console.CursorTop);
+    Console.Write(nameMatrixOne);
+    Console.SetCursorPosition((arrayToPrintMatrixTwo.GetLength(1)*6+10)-nameMatrixTwo.Length,Console.CursorTop);
+    Console.WriteLine(nameMatrixTwo);
     for (int i = 0; i < arrayToPrintMatrixOne.GetLength(0); i++)
-    {
+    {  
+        Console.Write("|");
         for (int j = 0; j < arrayToPrintMatrixOne.GetLength(1); j++)
         {
-            Console.Write($"{arrayToPrintMatrixOne[i, j]} ");
-            Console.Write(" | ");
-            for (j = 0; j < arrayToPrintMatrixTwo.GetLength(1); j++)
-            {
-                Console.Write($"{arrayToPrintMatrixTwo[i, j]}\t");
-            }
+            Console.Write($" {arrayToPrintMatrixOne[i, j]} "); 
         }
-        Console.WriteLine();
+        Console.Write("|   |");
+        for (int j = 0; j < arrayToPrintMatrixTwo.GetLength(1); j++)
+        {
+            Console.Write($" { arrayToPrintMatrixTwo[i, j]} ");
+        }
+        Console.Write("|");
+        System.Console.WriteLine();
     }
 }
-
+void PrintMatrixMulti(int[,] arrayToPrint,string name)              
+{
+    Console.SetCursorPosition((arrayToPrint.GetLength(1)*3/2)+arrayToPrint.GetLength(1)*3-name.Length/2,Console.CursorTop);  
+    Console.WriteLine(name);
+    for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+    {  Console.SetCursorPosition((arrayToPrint.GetLength(1)*3)/2,Console.CursorTop);
+        Console.Write("|");
+        for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+        {
+            Console.Write($" {arrayToPrint[i, j]} "); 
+        }
+        Console.Write("|");
+        Console.WriteLine();
+    }
+       
+}
 //Произведение матриц
 int[,] MultiplicationMatrix(int[,] MatrixOne,int[,] MatrixTwo)
 {
@@ -52,7 +76,6 @@ var MatrixOne = new int[sizeMatrix, sizeMatrix];
 var MatrixTwo = new int[sizeMatrix, sizeMatrix];
 int[,] MatrixArrOne = NumberArray(MatrixOne);
 int[,] MatrixArrTwo = NumberArray(MatrixTwo);
-PrintArray(MatrixArrOne,MatrixArrTwo);
+PrintMatrixs(MatrixArrOne,MatrixArrTwo,"Matrix1","Matrix2");
 int[,] result = MultiplicationMatrix(MatrixArrOne, MatrixArrTwo);
-//PrintArray(result,"Произведение матриц");
-Console.ReadLine();
+PrintMatrixMulti(result, "Result");
